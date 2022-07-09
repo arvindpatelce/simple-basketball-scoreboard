0cd6501bb30fe3c3a74f0b26b63f4ed94dae36b7
Arvind Patel <arvindpatelce@gmail.com>
2022-07-09 15:38:13 +0530

    Edited all files

---
+++ index.js
@@ -0,0 +1,32 @@
+let homeStoreEl =document.getElementById("home-score")
+let homeScore = 0
+
+function increaseHomeScoreOne(){
+    homeScore += 1
+    homeStoreEl.textContent = homeScore
+}
+
+function increaseHomeScoreTwo() {
+    homeScore += 2
+    homeStoreEl.textContent = homeScore
+}
+function increaseHomeScoreThree() {
+    homeScore += 3
+    homeStoreEl.textContent = homeScore
+}
+let guestStoreEl =document.getElementById("guest-score")
+let guestScore = 0
+
+
+function increaseGuestScoreOne() {
+    guestScore += 1
+    guestStoreEl.textContent = guestScore
+}
+function increaseGuestScoreTwo() {
+    guestScore += 2
+    guestStoreEl.textContent = guestScore
+}
+function increaseGuestScoreThree() {
+    guestScore += 3
+    guestStoreEl.textContent = guestScore
+}
